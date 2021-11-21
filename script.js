e23019b5360b34cabef47743d7d433e4c76db309
Suraj Khanal <58101903+surjkhanal@users.noreply.github.com>
2021-11-21 16:24:33 +0530

    api added

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-let featureBlock = document.getElementById('feature')
-let resultBlock = document.getElementById('result')
-let resultTxtBlock = document.getElementById('result-txt')
+let query = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
+makeAPICall(query).then(showResults);
 
+// let searchForm = document.forms['search-form-body']
+// searchForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     let text = event.target['search-txt'].value
+//     makeAPICall(query + text).then(showResults);
+// })
 
-function toggleResult(textRead){
-    feature.style.display = "none";
-    resultBlock.style.display = "block";
-    resultTxtBlock.textContent = textRead;
-    console.log(textRead);
-    console.log(resultTxtBlock.textContent);
-}
-let form = document.forms['file-form']
-form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    let inputFile = event.target.file.files[0];
-    if(inputFile && inputFile.type==="text/plain"){
-        console.log(inputFile);
-
-        let textRead = '';
-
-        let fReader= new FileReader()
-        fReader.readAsText(inputFile);
-
-        fReader.onload = function() {
-            textRead=fReader.result;
-            toggleResult(textRead)
-        };
-        
-        fReader.onerror = function() {
-            textRead=fReader.error;
-            toggleResult(textRead)
-          };
-
-
-
-
-    }
+document.querySelector('#search-txt').addEventListener('input', (event) => {
+    let text = event.target.value
+    makeAPICall(query + text).then(showResults);
+    document.querySelector('#result-conatiner h1').innerHTML="Result ..."
 })
+function showResults(data) {
+    let resultBlock = document.querySelector('#result-conatiner #result')
+    resultBlock.innerHTML = ''
+
+    data['meals'].forEach(meal => {
+        resultBlock.innerHTML += `
+        <a href="/food.html?foodId=${meal.idMeal}"> 
+            <div class = "food-block" >
+            <img src = ${meal.strMealThumb+ "/preview"} alt = "" >
+        <span > ${meal.strMeal} </span></div ></a> `
+    })
+}
+async function makeAPICall(req) {
+    let res = await fetch(req)
+    let data = await res.json()
+    return data;
+}
